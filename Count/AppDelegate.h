a972899824ebbb91318bed6546e6373791bac98d
田口うらら <UraraTaguchi@taguchiurara-no-MacBook-Pro.local>
2015-02-10 16:55:35 +0900

    Initial Commit

---
+++ Count/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Count
+//
+//  Created by 田口うらら on 2015/02/10.
+//  Copyright (c) 2015年 田口うらら. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Count/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Count
+//
+//  Created by 田口うらら on 2015/02/10.
+//  Copyright (c) 2015年 田口うらら. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
